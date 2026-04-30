31cbf625d7ea47c46e21b1e7a583954493bd875b
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2026-04-30 20:43:38 +0200

    fix qr-code

--- backend/server.js
+++ backend/server.js
@@ -100,6 +100,8 @@ const {
 const app = express();
 app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
+app.use(express.static('crm-panel'));
+
 
 app.use((req, res, next) => {
     console.log(`📨 ${req.method} ${req.url}`);
